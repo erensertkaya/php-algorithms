8d4597da084b6f440888c2eb60ddec50ebd5be51
Eren <eren.sertkaya.es@gmail.com>
2024-01-06 23:44:22 +0300

    feat: add linear search

--- search/binary.php
+++ search/binary.php
@@ -1,1 +1,27 @@
 <?php
+
+$sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function binarySearch($haystack,$needle): int
+{
+    $high = count($haystack)-1;
+    $low = 0;
+
+
+    while ($low <= $high){
+        $mid = floor(($low + $high) / 2);
+        if ($haystack[$mid] == $needle){
+            return $mid;
+        }
+        elseif ($haystack[$mid] < $needle){
+            $low = $mid+1;
+        }
+        else{
+            $high = $mid-1;
+        }
+    }
+
+    return false;
+}
+
+echo binarySearch($sortedArray,9);
--- search/linear.php
+++ search/linear.php
@@ -1,1 +1,14 @@
 <?php
+$array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function linearSearch($haystack,$needle,$typeIntensive = false)
+{
+    foreach ($haystack as $key => $hay){
+        if ($typeIntensive && $hay === $needle){
+            return $key;
+        }
+    }
+    return false;
+}
+
+echo linearSearch($array,"2");
